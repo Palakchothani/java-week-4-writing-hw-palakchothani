3d4e683bd393cf18767ca522a248d0530feb038a
Palakchothani <palakchothani11@gmail.com>
2022-11-04 19:22:16 +0000

    homework_week_8

---
+++ src/programme_23_method_overriding/Vehicle.java
@@ -0,0 +1,9 @@
+package programme_23_method_overriding;
+
+public class Vehicle {
+    //defining a method
+    public void run() {
+        System.out.println("Vehicle is running");
+    }
+}
+
---
+++ src/programme_24_method_overriding/Axis.java
@@ -0,0 +1,7 @@
+package programme_24_method_overriding;
+
+public class Axis {
+    public int getRateOfInterest() {
+        return 9;
+    }
+}
